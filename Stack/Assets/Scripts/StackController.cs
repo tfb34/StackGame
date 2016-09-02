f3eecfc5a4b7f5638b7dc897e7331b7a8e9771b6
Thalia <thalia.brito@rutgers.edu>
2016-09-02 14:26:34 -0400

    stackController script. taking from bottom of stack and placing it on top of the stack whenever player left clicks on mouse

---
+++ Stack/Assets/Scripts/StackController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StackController : MonoBehaviour {
+    private GameObject[] stack;
+    private int stackIndex;
+    private int scoreCount = 0;
+	// Use this for initialization
+	void Start () {
+        stack = new GameObject[transform.childCount];
+        stackIndex = stack.Length-1;
+        
+        //adding objects into the stack array
+        for (int i =0;i<transform.childCount;i++)
+        {
+            stack[i] = transform.GetChild(i).gameObject;
+        }
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))// left click
+        {
+            AddTile();
+        }
+	}
+    private void AddTile()
+    {
+        stack[stackIndex].transform.localPosition = new Vector3(0, scoreCount, 0);
+    }
+}
